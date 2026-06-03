45569d7437f7146b6580f1fdc2043af5d45b2b69
Cousin Ze <alex@ckx-ily.cn>
2026-06-03 17:15:57 +0800

    Summoning Minions in 30 seconds

---
+++ duelx-core/src/main/java/club/doki7/duelx/core/entity/HumanEntity.java
@@ -0,0 +1,7 @@
+package club.doki7.duelx.core.entity;
+
+public final class HumanEntity extends AbstractEntity {
+    HumanEntity(String name) {
+        super(name);
+    }
+}
---
+++ duelx-core/src/main/java/club/doki7/duelx/core/entity/MinionEntity.java
@@ -0,0 +1,7 @@
+package club.doki7.duelx.core.entity;
+
+public final class MinionEntity extends AbstractEntity {
+    MinionEntity(String name) {
+        super(name);
+    }
+}
